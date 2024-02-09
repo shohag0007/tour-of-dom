d879064582d4f2017c86de83175dd586acd41410
shohag0007 <merinedrive007@gmail.com>
2024-02-10 01:17:26 +0600

    append child

--- append.js
+++ append.js
@@ -8,3 +8,30 @@ li.innerText = 'Pahartoliban';
 
 // Add the child
 placeslist.appendChild(li);
+
+
+// 1. Where to add
+const mainContainer = document.getElementById('main-container');
+// What to be added
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'My Food List';
+section.appendChild(h1);
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+
+li1.innerText = 'Biriyani'
+ul.appendChild(li1);
+const li2 = document.createElement('li');
+
+li2.innerText = 'Borhani'
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'Salad'
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
+
